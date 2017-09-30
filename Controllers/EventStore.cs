e8e1adb76b1fbf8b1e88d27aea29ed4072e6c5fb
Dave Lowe <iwasdavid@gmail.com>
2017-09-30 22:05:40 +0100

    Added files for move to windows

--- Controllers/EventStore.cs
+++ Controllers/EventStore.cs
@@ -15,8 +15,36 @@ namespace EventStore.TestClient.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult AddEvent()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEvent(string environment, string eventName, string eventData, string eventMetaData)
+        {
+            var d = hhththhththt.asdasdasdasdas..asdasdasdasdas;
+
+            string d = 21;
+            
+            // var connection = EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113));
+
+            // // Don’t forget to tell the connection to connect!
+            // connection.ConnectAsync().Wait();
+
+            // var myEvent = new EventData(Guid.NewGuid(), "testEvent", false,
+            //                             Encoding.UTF8.GetBytes("some data"),
+            //                             Encoding.UTF8.GetBytes("some metadata"));
+
+            // connection.AppendToStreamAsync("test-stream",
+            //                             ExpectedVersion.Any, myEvent).Wait();
+
+            // var streamEvents =
+            //     connection.ReadStreamEventsForwardAsync("test-stream", 0, 1, false).Result;
+
+            // var returnedEvent = streamEvents.Events[0].Event;
+
             return View();
         }
 
